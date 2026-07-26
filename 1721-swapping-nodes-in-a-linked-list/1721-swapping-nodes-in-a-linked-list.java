f9734965d32d256ba337e217278b45a0c608153f
Avninder-kaur <kauravninder60@gmail.com>
2026-07-26 16:32:04 +0530

    Time: 2 ms (52.79%), Space: 119.2 MB (33.54%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -10,19 +10,42 @@
  */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-        ListNode first = head;
-        for(int i = 0; i < k - 1; i++){
-            first = first.next;
+        // ListNode first = head;
+        // for(int i = 0; i < k - 1; i++){
+        //     first = first.next;
+        // }
+        // ListNode slow = head;
+        // ListNode fast = first;
+        // while(fast.next != null){
+        //     slow = slow.next;
+        //     fast = fast.next;
+        // }
+        // int temp = slow.val;
+        // slow.val = first.val;
+        // first.val = temp;
+        // return head;
+
+        ListNode curr = head;
+        for (int i = 1; i < k; i++) {
+            curr = curr.next;
         }
-        ListNode slow = head;
-        ListNode fast = first;
-        while(fast.next != null){
-            slow = slow.next;
-            fast = fast.next;
+        ListNode first=curr;
+        curr = head;
+        int count = 0;
+        while (curr != null) {
+            count++;
+            curr = curr.next;
         }
-        int temp = slow.val;
-        slow.val = first.val;
-        first.val = temp;
+        int moves=count-k;
+        curr=head;
+        while(moves>0){
+            curr=curr.next;
+            moves--;
+        }
+        ListNode second=curr;
+        int temp=first.val;
+        first.val=second.val;
+        second.val=temp;
         return head;
     }
 }
